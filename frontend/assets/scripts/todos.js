7d883aa55e26e830aa1d40dfc1d9983600ce1bb4
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-16 22:36:04 +0200

    [UPDATE] todos.js
    made the due time readdable

--- frontend/assets/scripts/todos.js
+++ frontend/assets/scripts/todos.js
@@ -51,11 +51,22 @@ async function loadTodos() {
 
     todos.forEach(todo => {
       const row = document.createElement('tr');
+      const date = new Date(todo.dtime);
+
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const day = date.getDate().toString().padStart(2, '0');
+
+      const hour = date.getHours().toString().padStart(2, '0');
+      const min = date.getMinutes().toString().padStart(2, '0');
+      const sec = date.getSeconds().toString().padStart(2, '0');
+
+      const dueTime = `${day}/${month}/${year} at ${hour}:${min}:${sec}`
 
       row.innerHTML = `
             <td>${todo.title}</td>
             <td>${todo.descr}</td>
-            <td>${todo.dtime}</td>
+            <td>${dueTime}</td>
             <td>${todo.status}</td>
           `;
       const button = document.createElement('div');
